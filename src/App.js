8caecb5426c21632e953f721fe02ac4eec0d9d3f
Pierre Lasante <pierre.lasante@videotron.ca>
2023-06-03 07:18:10 -0700

    Adds Bootstrap 5.3.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          We made our first React Application!
-        </p>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header'>
+          Your Todo's
+        </div>
+        <div className='card-body'>
+          <table className='table table-hover'>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Feed dog</td>
+                <td>Eric</td>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <td>Get haircut</td>
+                <td>Eric</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
